a9d7996ab1311c6d066c76b6a0be69154673abf4
icoder <renqingwei123@163.com>
2024-04-07 15:29:11 +0800

    sync with db9960fcbef23b9184112a046ab4e024e01d258d: Ensure wasm library builds correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 #[macro_use]
-extern crate maplit;
-#[macro_use]
 extern crate log;
 
+#[macro_use]
+mod mac;
+
+mod cnf;
 mod ctx;
 mod dbs;
 mod doc;
 mod err;
 mod fnc;
+mod key;
 mod kvs;
 mod sql;
 
 fn main() {
-	// cli::init(); // Initiate the command line
+	// TODO: implement initiation from wasm
 }
