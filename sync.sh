010d6881c6ab268eeff95f97253b624a670d5334
icoder <renqingwei123@163.com>
2024-04-07 21:20:56 +0800

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+rm -rf app/
 rm -rf src/
+rm -rf lib/
+cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/app .
 cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/src .
+cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/lib .
