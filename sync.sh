bc2a4a9f38962d1f3daf43f7c9d0c351fa13d3b7
icoder <renqingwei123@163.com>
2024-04-08 18:02:30 +0800

    sync 8e283ecaeb1597f7b418c335c64f027fa38e699d: Only check fields if the document is a table record

--- sync.sh
+++ sync.sh
@@ -3,6 +3,8 @@
 rm -rf app/
 rm -rf src/
 rm -rf lib/
+rm Makefile
 cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/app .
 cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/src .
 cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/lib .
+cp -r /Users/qingweiren/code/db/surrealdb/surrealdb/Makefile .
